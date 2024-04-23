5f3fd6c76da39a1514e59ec96501a60316480055
monsecabral <cabral_monse@yahoo.com>
2024-04-23 15:16:24 -0400

    checkpoint

---
+++ demo 2-8.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var num;
+
+num = 5;
+
+
+console.log("The new value of num is: " + num)
+
+num = num + 1;
+
+
+console.log("The new value of num is: " + num)
---
+++ demo-2-5.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var num1 =
+var num2 =
+var num3 =
+
+num1 = 12;
+num2 = -10;
+
+num3 = 7;
+
+num4 = 42;
+
+var answer1 = num2 / 2;
+
+console.log(answer1);
+
+consolge.log(-answer1);
+
+console.log(answer1);
+
+answer1 = -answer1;
+
+console.log(answer1);
